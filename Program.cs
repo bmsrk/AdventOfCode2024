8caa68a0a31790067c1571a02629afbc194fd681
bruno <bruno@BMSRKDELLG15>
2024-12-10 11:42:26 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2024.Day1;
+
+Console.WriteLine("advent of code 2024 by bmsrk");
+
+new Day1().Part1();
+new Day1().Part2();
+
